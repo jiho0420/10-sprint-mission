3033c10e65af39582ede39b823c65e8650b8629f
jiho0420 <jiho0420@gachon.ac.kr>
2026-01-27 18:37:12 +0900

    sprint3 commit

--- src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
+++ src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
@@ -10,34 +10,39 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
-import java.util.UUID;
 
 @SpringBootApplication
 public class DiscodeitApplication {
+
+    static User setupUser(UserService userService) {
+        User user = userService.create("woody", "woody@codeit.com", "woody1234");
+        return user;
+    }
+
+    static Channel setupChannel(ChannelService channelService) {
+        Channel channel = channelService.create(add .ChannelType.PUBLIC, "공지", "공지 채널입니다.");
+        return channel;
+    }
+
+    static void messageCreateTest(MessageService messageService, Channel channel, User author) {
+        Message message = messageService.create("안녕하세요.", channel.getId(), author.getId());
+        System.out.println("메시지 생성: " + message.getId());
+    }
 
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(DiscodeitApplication.class, args);
 
         UserService userService = context.getBean(UserService.class);
         ChannelService channelService = context.getBean(ChannelService.class);
-        MessageService messageService = context.getBean(MessageService.class);
+        MesseageService messageService = context.getBean(MessageService.class);
 
-        System.out.println("========== 스프링 부트 기반 테스트 시작 ==========\n");
+        System.out.println("테스트 시작\n");
+
         User user = setupUser(userService);
         Channel channel = setupChannel(channelService);
         messageCreateTest(messageService, channel, user);
+
     }
 
-    static User setupUser(UserService userService) {
-        return userService.createUser("woody", "woody1234", "woody@codeit.com");
-    }
 
-    static Channel setupChannel(ChannelService channelService) {
-        return channelService.createChannel("공지");
-    }
-
-    static void messageCreateTest(MessageService messageService, Channel channel, User user) {
-        Message message = messageService.createMessage("안녕하세요.", channel.getId(), user.getId());
-        System.out.println("메시지 생성 확인: " + message.getContent());
-    }
 }
