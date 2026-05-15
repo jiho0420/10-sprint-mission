250480acb694cf5f4ebafa7dd5f0983464763670
jiho0420 <jiho0420@gachon.ac.kr>
2026-05-15 18:08:31 +0900

    기존 로그인 관련 코드 삭제 및 리팩토링

--- src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -1,47 +1,19 @@
 package com.sprint.mission.discodeit.controller;
 
-import com.sprint.mission.discodeit.dto.LoginRequestDto;
-import com.sprint.mission.discodeit.dto.UserDto;
-import com.sprint.mission.discodeit.service.AuthService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.ExampleObject;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.web.csrf.CsrfToken;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @Slf4j
 @Tag(name = "Auth")
 @RestController
-@RequiredArgsConstructor
 @RequestMapping("/api/auth")
 public class AuthController {
-    private final AuthService authService;
-
-    @Operation(summary = "로그인", operationId = "login")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "로그인 성공"),
-            @ApiResponse(responseCode = "400", description = "비밀번호가 일치하지 않음",
-                    content = @Content(examples = @ExampleObject(value = "Wrong password"))),
-            @ApiResponse(responseCode = "404", description = "사용자를 찾을 수 없음",
-                    content = @Content(examples = @ExampleObject(value = "User with username {username} not found")))
-    })
-    @RequestMapping(method = RequestMethod.POST, value = "/login")
-    public UserDto login(@Valid @RequestBody LoginRequestDto request){
-        return authService.login(request);
-    }
 
     @RequestMapping(method = RequestMethod.GET, value = "/csrf-token")
     public ResponseEntity<Void> getCsrfToken(CsrfToken csrfToken) {
--- src/main/java/com/sprint/mission/discodeit/service/AuthService.java
+++
@@ -1,9 +0,0 @@
-package com.sprint.mission.discodeit.service;
-
-import com.sprint.mission.discodeit.dto.LoginRequestDto;
-import com.sprint.mission.discodeit.dto.UserDto;
-
-@FunctionalInterface
-public interface AuthService {
-    UserDto login(LoginRequestDto request);
-}
